dacf4c17b7687605f123544c76f7abc2a15a6390
elkon91 <elkon91@gmail.com>
2024-10-01 12:13:29 +0100

    Revert "MY_CALC: added multiply function"
    This reverts commit 7995a6b3d72ef067295eea2159c0e58b5c20aa65.

--- my_calc.py
+++ my_calc.py
@@ -6,9 +6,3 @@ def add_numbers(num_1, num_2):
 def subtract_numbers(num_1, num_2):
     result = num_1 - num_2
     return result 
-
-def multiply_numbers(num_1, num_2):
-    while True:
-        result = num_1 * num_2
-    
-    return result 
